00b2bcf2b2b39eb31be75df54133cf480b7892b0
Aleksandr.i <aleksandr.i@roxot.com>
2016-11-10 14:01:36 +0300

    Create project

---
+++ app/apprun.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksandr.i
+ * Date: 10.11.16
+ * Time: 13:37
+ */
+
+namespace Finance\application;
+
+
+class apprun
+{
+
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksandr.i
+ * Date: 10.11.16
+ * Time: 12:58
+ */
